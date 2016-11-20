27a1b8f46283896204706bcbb83bf8da2e2a381b
Jacob Keith <jacobkeith42@gmail.com>
2016-11-20 16:44:07 -0500

    Added OceanOptics extraction

--- TraceSpectralPowerDistribution.m
+++ TraceSpectralPowerDistribution.m
@@ -1,5 +1,5 @@
 function TraceSpectralPowerDistribution(instrument,output)
-    [FileName,PathName] = uigetfile('*.jpg','Select Images...','image.jpg','MultiSelect','on');
+    [FileName,PathName] = uigetfile('*.*','Select Images...','image.jpg','MultiSelect','on');
     
     heading = {'Wavelength (nm)'};
     for i=1:length(FileName)
@@ -9,22 +9,37 @@ function TraceSpectralPowerDistribution(instrument,output)
     
     
     alldata = [];
-    if instrument == 'sekonic'
+    switch instrument 
         
-        step = 400/1699;
-        plotX = [380+step:step:780]';
-        alldata(:,1) = plotX;
+        case 'sekonic'
+            
+            step = 400/1699;
+            plotX = [380+step:step:780]';
+            alldata(:,1) = plotX;
+
+            for i=1:length(FileName)
+                
+                imagepath = strcat(PathName,FileName{i});
+                trace = processSekonic(imagepath);
+                alldata(:,i+1) = trace;
+            end
         
-        for i=1:length(FileName)
-            imagepath = strcat(PathName,FileName{i});
-            trace = processSekonic(imagepath);
-            alldata(:,i+1) = trace;
-        end
-    elseif instrument == 'oceanoptics'
+        case 'oceanoptics'
         
-    elseif instrument == 'asensetek'
+            step = 420/765;
+            plotX = [380+step:step:800]';
+            alldata(:,1) = plotX;
+
+            for i=1:length(FileName)
+                imagepath = strcat(PathName,FileName{i});
+                trace = processOceanoptics(imagepath);
+                alldata(:,i+1) = trace;
+            end
+
+        case 'asensetek'
         
     end
+    
     alldata = num2cell(alldata);
     finalexport = [heading; alldata];
     size(finalexport)
@@ -34,6 +49,9 @@ end
 
 function trace = processSekonic(imagepath)
     
+    % 380 to 780 nm
+    % 1699 datapoints
+    
     image = imread(imagepath);
     graphCrop = imcrop(image,[181 58 1698 1147]);
     
@@ -43,7 +61,7 @@ function trace = processSekonic(imagepath)
         thiscol = distribution(:,i);
         nonzero = find(thiscol,1);
         if isempty(nonzero)
-            trace(i) = 0;
+            trace(i) = NaN;
         else
             trace(i) = 1 - nonzero/size(distribution,1);
         end
@@ -52,18 +70,24 @@ end
 
 function trace = processOceanoptics(imagepath)
     
-    image = imread(imagepath);
-    graphCrop = imcrop(image,[181 58 1698 1147]);
+    % 380 to 800nm
+    % 765 datapoints
     
-    distribution = rgb2gray(graphCrop) < 250 & rgb2gray(graphCrop) > 5;
-    trace = zeros(1,size(distribution,2));
-    for i=1:size(distribution,2)
-        thiscol = distribution(:,i);
+    image = imread(imagepath);
+    graphCrop = imcrop(image,[223 252 764 411]);
+    pixelmaskPurple = graphCrop(:,:,1) == 255 & graphCrop(:,:,2) == 0 & graphCrop(:,:,3) == 255;
+    pixelmaskGreen = graphCrop(:,:,1) == 21 & graphCrop(:,:,2) == 142 & graphCrop(:,:,3) == 21;
+    pixelmask = pixelmaskPurple | pixelmaskGreen;
+    
+    for i=1:size(pixelmask,2)
+        thiscol = pixelmask(:,i);
         nonzero = find(thiscol,1);
         if isempty(nonzero)
             trace(i) = 0;
         else
-            trace(i) = 1 - nonzero/size(distribution,1);
+            trace(i) = 1 - nonzero/size(pixelmask,1);
         end
     end
+    
+    trace = imadjust(trace,[min(trace);max(trace)],[0,1]);
 end
